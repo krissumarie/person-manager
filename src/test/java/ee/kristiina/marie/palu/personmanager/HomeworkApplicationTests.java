f5c62066e01a33944c37f3c64bbc88ae57540bf8
krissumarie <krmpalu@gmail.com>
2025-04-24 16:17:34 +0300

    Initial commit

---
+++ src/test/java/ee/kristiina/marie/palu/personmanager/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kristiina.marie.palu.personmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
